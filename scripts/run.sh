c113f33899f4c54be1d51953353d7848699db451
danielg-favero <danielfavero17@gmail.com>
2022-05-18 20:25:25 -0300

    fix: alterado script de execução

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,14 @@
 clear
+
 cd analisador_sintatico/src
 bison -dy ansin.y
-mv y.tab.c ../../analisador_lexico/src
 mv y.tab.h ../../analisador_lexico/src
+mv y.tab.c ../../analisador_lexico/src
+
 cd ../../analisador_lexico/src
 flex anlex.l
-gcc -o output lex.yy.c y.tab.c
+gcc -o output lex.yy.c y.tab.c -lfl
+mv output ../../
+
+cd ../../
 ./output $1
-cd ../..
